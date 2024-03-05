fa7f38cc9c417e38c31f6afe55d7feefd7babb98
Robert-Vardanyan <robertvardanyan22@icloud.com>
2024-03-05 17:56:31 +0400

    Towers of Hanoi v.1.5

--- towers_of_hanoi.py
+++ towers_of_hanoi.py
@@ -1,36 +1,37 @@
 ### Rova
 ### Smart assistant for the game 'Towers of Hanoi'
-### Version 1.2
+### Version 1.5
+print("\n👋😄 Hello, I'm the Smart Assistant created by #Rova to help you figure out the steps needed\n\t\tto win as quickly as possible in the game 'Towers of Hanoi'.\n")
 def arajin_erkrord(syun_1,syun_2,n_qayly):
     if not syun_2:
         syun_2.append(syun_1.pop(-1))
-        return f'{n_qayly:3} | {syun_2[-1]} 1-->2\n'
+        return f'{n_qayly:3}) {syun_2[-1]} |1 --> 2|'
     elif not syun_1:
         syun_1.append(syun_2.pop(-1))
-        return f'{n_qayly:3} | {syun_1[-1]} 2-->1\n'
+        return f'{n_qayly:3}) {syun_1[-1]} |2 --> 1|'
     elif tarer.index(syun_1[-1]) < tarer.index(syun_2[-1]):
         syun_2.append(syun_1.pop(-1))
-        return f'{n_qayly:3} | {syun_2[-1]} 1-->2\n'
+        return f'{n_qayly:3}) {syun_2[-1]} |1 --> 2|'
     elif tarer.index(syun_1[-1]) > tarer.index(syun_2[-1]):
         syun_1.append(syun_2.pop(-1))
-        return f'{n_qayly:3} | {syun_1[-1]} 2-->1\n'
+        return f'{n_qayly:3}) {syun_1[-1]} |2 --> 1|'
 def arajin_errord(syun_1,syun_3,n_qayly):
     if not syun_3:
         syun_3.append(syun_1.pop(-1))
-        return f'{n_qayly:3} | {syun_3[-1]} 1-->3\n'
+        return f'{n_qayly:3}) {syun_3[-1]} |1 --> 3|'
     elif tarer.index(syun_1[-1]) < tarer.index(syun_3[-1]):
         syun_3.append(syun_1.pop(-1))
-        return f'{n_qayly:3} | {syun_3[-1]} 1-->3\n'
+        return f'{n_qayly:3}) {syun_3[-1]} |1 --> 3|'
     elif tarer.index(syun_1[-1]) > tarer.index(syun_3[-1]):
         syun_1.append(syun_3.pop(-1))
-        return f'{n_qayly:3} | {syun_1[-1]} 3-->1\n'
+        return f'{n_qayly:3}) {syun_1[-1]} |3 --> 1|'
 def erkrord_errord(syun_2,syun_3,n_qayly):
     if tarer.index(syun_2[-1]) < tarer.index(syun_3[-1]):
         syun_3.append(syun_2.pop(-1))
-        return f'{n_qayly:3} | {syun_3[-1]} 2-->3\n'
+        return f'{n_qayly:3}) {syun_3[-1]} |2 --> 3|'
     elif tarer.index(syun_2[-1]) > tarer.index(syun_3[-1]):
         syun_2.append(syun_3.pop(-1))
-        return f'{n_qayly:3} | {syun_2[-1]} 3-->2\n' 
+        return f'{n_qayly:3}) {syun_2[-1]} |3 --> 2|' 
 def move(n):
     s = 0
     qaylq = ['1','2','3']
@@ -38,8 +39,7 @@ def move(n):
     if n % 2 == 0:
         while True:
             if len(syun_3) == n:
-                print(f'Finish\nn = {n}\nsteps((2 ** {n}) - 1) = {n_qayly}')
-                break
+                return f'\n🏁🏁🏁 Finish 🏁🏁🏁\n\n⭕️ Rings: {n}\n🥏 Total count of steps:  {n_qayly}\n🤓 Formula for calculating the number of steps:\t( (2^{n}) - 1 )'
             else:
                 if qaylq[s] == '1':
                     s += 1
@@ -56,8 +56,7 @@ def move(n):
     elif n % 2 != 0:
         while True:
             if len(syun_3) == n:
-                print(f'Finish\nn = {n}\nsteps((2 ** {n}) - 1) = {n_qayly}')
-                break
+                return f'\n🏁🏁🏁 Finish 🏁🏁🏁\n\n⭕️ Rings: {n}\n🥏 Total count of steps:  {n_qayly}\n🤓 Formula for calculating the number of steps:\t( (2^{n}) - 1 )'
             else:
                 if qaylq[s] == '1':
                     s += 1
@@ -71,10 +70,11 @@ def move(n):
                     s = 0
                     n_qayly += 1
                     print(erkrord_errord(syun_2,syun_3,n_qayly))    
-n = int(input('Input n: '))
+n = int(input('Count of rings: '))
 if n < 1 or n > 52:
-    print('Wrong input!\nmin(n) = 1\nmax(n) = 52')
+    print('\n   ❌ Wrong input ❌\n❌ Minimum count = 1 ❌\n❌ Maximum count = 52❌')
     exit()
+print('Move the rings according to the numbered steps:\n')
 syun_1,syun_2,syun_3 = [],[],[]
 tarer = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 for i in range(n):
